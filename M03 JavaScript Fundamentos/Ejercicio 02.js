f32aae6d046789a3b756279f5fc300764701be44
MIsabelCamacho <maisacl2015@gmail.com>
2023-08-08 17:43:56 -0400

    commit Isabel homeworkM03

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -1,9 +1,11 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
-function devolverString(string) {
+function devolverString(string) 
+{
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +13,54 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var resultado = x + y;
+   return resultado;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   result = x-y;
+   return result;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var division = 0;
+   if(x>=y)
+   {
+      division = x/y;
+      return division;
+   }
+   else
+   {
+      division = y/x;
+      return division;
+   }
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var multiplicacion= x * y;
+   return multiplicacion;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var resto = 0;
+   if(x>=y)
+   {
+      resto = x % y;
+      return resto;
+   }
+   else
+   {
+      resto = y % x;
+      return resto;
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
